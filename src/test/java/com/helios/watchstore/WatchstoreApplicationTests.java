a86c1485b2d41e97c69cc397473a04a7d694cc52
Omkar Birade <omkarbirade20@gmail.com>
2020-03-27 13:47:28 +0530

    created a starter spring boot web application

---
+++ src/test/java/com/helios/watchstore/WatchstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helios.watchstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
