e28ea9479c5a3da17956f5851c29be1f63b91e2a
Omkar Birade <omkarbirade20@gmail.com>
2020-03-28 23:47:56 +0530

    changed Generation type to identity from auto as auto defaults to SequenceGenerator and if it fails to find SequenceGenerator hibernate table it fails trying to add null as primary key

--- src/main/java/com/helios/watchstore/models/Product.java
+++ src/main/java/com/helios/watchstore/models/Product.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 @Table(name = "products")
 public class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     @Column(name = "description")
