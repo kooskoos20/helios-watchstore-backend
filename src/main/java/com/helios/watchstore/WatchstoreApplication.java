2363ff5beef059d90dc3a9b95578dc373c34df60
Omkar Birade <omkarbirade20@gmail.com>
2020-03-28 12:41:37 +0530

    added service for product

--- src/main/java/com/helios/watchstore/WatchstoreApplication.java
+++ src/main/java/com/helios/watchstore/WatchstoreApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication	// same as @Configuration @EnableAutoConfiguration @ComponentScan, @Configuration scans for being definitions in the class and makes sures only one instance is created of the bean
 public class WatchstoreApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WatchstoreApplication.class, args);
 	}
--- src/main/java/com/helios/watchstore/services/ProductService.java
+++ src/main/java/com/helios/watchstore/services/ProductService.java
@@ -1,4 +1,22 @@
 package com.helios.watchstore.services;
 
+import com.helios.watchstore.repositories.ProductRepository;
+import com.helios.watchstore.models.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductService {
+    private ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts(){
+        return productRepository.findAll();
+    }
 }
