58de7b9c22bddc8ff6c0a6c07241f53e4d9c4d56
Omkar Birade <omkarbirade20@gmail.com>
2020-03-28 13:37:43 +0530

    Intialising seed data using data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO products(id,title, description, image_url, image_thumbnail_url) values ('1','CITIZEN AQ ANALOG WATCH - FOR MEN (BLACK)', 'Citizen, a revered name in the ', 'ication/watch_1.jpeg', '   ch_1_thumbnail.jpeg');
--- src/main/resources/db/migration/V1__CreateTableProducts.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE products(id SERIAL PRIMARY KEY, title VARCHAR(500), description VARCHAR(10000), image_url VARCHAR(500), image_thumbnail_url VARCHAR(500));
